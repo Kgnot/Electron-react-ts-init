ce6fdc039f03950e1b8e7011f2040f24a2cb7f9c
Henry Ricaurte Mora <hricaurtem@proton.me>
2024-12-19 10:38:46 -0500

    Init2

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,9 +1,26 @@
 import {app, BrowserWindow} from 'electron';
 import path from 'path';
+import { exec } from 'child_process';
 
-//type test = string;
+
+const executeJar = ()=> {
+    const jarPath = path.join(__dirname , 'target/App.jar');
+
+    exec(`start java -jar ${jarPath}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error al ejecutar el .jar: ${error}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
 
 app.on("ready", () => {
+    executeJar();
     const mainWindow = new BrowserWindow({width: 800, height: 600});
     mainWindow.loadFile(path.join(app.getAppPath()) + "/dist-react/index.html"); //producción no?
 })
